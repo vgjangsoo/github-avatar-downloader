5487ec0121ea02b32c9af3fd197677bc186b8f1f
Sungil Saul Lee <jangsoo86@gmail.com>
2019-04-08 23:45:07 +0000

    Start the getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,20 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+  // request.get('https://github.com/nodejs/node')
+  //        .on('error', function(err) {
+  //         throw err;
+  //        })
+  //        .on('response', function(response) {
+  //         console.log(response['login']);
+  //        })
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
