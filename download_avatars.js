17bdf415b95198659c705b41bffa348258c1d74b
Sungil Saul Lee <jangsoo86@gmail.com>
2019-04-09 01:19:58 +0000

    Step 7: Print each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var secrets = require('./secrets')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -8,16 +9,22 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
+      'Authorization': 'Bearer ' + secrets['GITHUB_TOKEN']
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
   });
 }
 
 
+
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Errors:", err);
+  //console.log("Result:", result);
+
+  result.forEach(function(element) {
+    console.log(element.avatar_url);
+  });
 });
